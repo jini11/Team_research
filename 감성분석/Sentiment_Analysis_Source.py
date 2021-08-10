8f4b63d5db8bb2206772f5406ac581da9e22ea33
BeefLover <bejm1997@gmail.com>
2021-08-11 01:12:57 +0900

    또안되_2 주피터 노트북에서 무언가 오류가 난건가...

--- 감성분석/Sentiment_Analysis_Source.py
+++ 감성분석/Sentiment_Analysis_Source.py
@@ -1,6 +1,7 @@
 from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 import numpy as np
+import re
 
 corpus = [
     '푸짐하게 잘 먹었습니다.'
@@ -22,3 +23,18 @@ idf = np.log(D+1 / (df+1) + 1)
 tfidf = tf * idf
 
 tfidf = tfidf / np.linalg.norm(tfidf, axis = 1, keepdims = True)
+
+count = CountVectorizer()
+docs = np.array([
+        'The sun is shining',
+        'The weather is sweet',
+        'The sun is shining, the weather is sweet, and one and one is two'])
+bag = count.fit_transform(corpus)
+
+def preprocessor(corpus):
+    text = re.sub('<[^>]*>', '', corpust)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',
+                           corpus)
+   corpus = (re.sub('[\W]+', ' ', corpus.lower()) +
+            ' '.join(emoticons).replace('-', ''))
+    return corpus
