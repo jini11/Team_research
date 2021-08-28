03278cdea343f040b7e68f88ecdc66e641c74ac3
jini_11 <78905126+jini11@users.noreply.github.com>
2021-08-28 11:18:57 +0900

    차원축소 수정(t-SNE)

---
+++ Clustering/dbscan_ver3/clustering_dbscan_ver3_2.py
@@ -0,0 +1,100 @@
+#%%
+
+from inspect import EndOfBlock
+from os import name, sep
+import matplotlib
+from nltk import tokenize
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from konlpy.tag import Okt
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.cluster import DBSCAN
+from sklearn.metrics import silhouette_score, davies_bouldin_score
+from sklearn.manifold import TSNE
+
+
+
+df = pd.read_csv("C:/Users/User/OneDrive - 공주대학교/바탕 화면/review/맞춤법 정리(리뷰, 레이블).csv", encoding='cp949')
+
+train_x = df['리뷰']
+
+
+okt = Okt()
+print(train_x[4])
+print(okt.morphs(train_x[4]))
+
+#tf-idf
+tfidf = TfidfVectorizer(ngram_range=(1, 2), min_df=5, max_df=0.9, tokenizer=okt.morphs, token_pattern=None)
+tfidf.fit(train_x.values.astype('U'))
+train_x_okt = tfidf.transform(train_x.values.astype('U'))
+vector = tfidf.transform(train_x.values.astype('U')).toarray()
+
+
+#차원 축소(t-SNE)
+tsne = TSNE(n_components=2)
+
+#tsne_df = pd.DataFrame({'x': tsne[:,0], 'y': tsne[:,1], 'classes':df['긍/부정']})
+
+#plt.figure(figsize=(16,10))
+#sns.scatterplot(
+#    x='x', y='y',
+#    hue= 'classes',
+#    data = tsne_df,
+#    legend= "full",
+#    alpha =0.4
+#)
+
+#plt.title("tSNE")
+#plt.show()
+
+vector = tsne.fit_transform(vector)
+vector.shape
+
+#DBSCAN
+model = DBSCAN(eps=0.8, min_samples=4)
+
+vector = pd.DataFrame(vector)
+
+result = model.fit(vector)
+
+result_id = pd.DataFrame(result.labels_)
+
+
+
+d2 = pd.DataFrame()
+d2 = pd.concat([vector, result_id], axis=1)
+d2.columns = [0,1,"cluster"]
+
+
+
+#시각화
+sns.scatterplot(d2[0], d2[1], hue=d2['cluster'], style=d2['cluster'], legend="full")
+plt.title('DBSCAN')
+plt.show()
+
+
+plt.clf()
+plt.close()
+
+print('Silhouette Coefficient: {:.4f}'.format(silhouette_score(d2.iloc[:,:-1], d2['cluster'])))
+print('Davies Bouldin Index: {:.4f}'.format(davies_bouldin_score(d2.iloc[:,:-1], d2['cluster'])))
+
+df['result'] = result_id
+
+result_id = np.array(result_id).flatten().tolist()
+
+
+f = open('C:/Users/User/OneDrive - 공주대학교/바탕 화면/review/result_dbscan_ver3.txt',mode='w', encoding='utf-8' )
+
+for cluster_num in set(result_id):
+    f.write("cluster num : {} \n".format(cluster_num))
+    temp_df = df[df['result'] == cluster_num]
+    for review in temp_df['리뷰']:
+        f.write("{}".format(review))
+        f.write('\n')
+
+f.close
+# %%
