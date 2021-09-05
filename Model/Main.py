71acb5516062512c78ece5e2e571c0f5093e3182
jini_11 <78905126+jini11@users.noreply.github.com>
2021-09-05 17:23:34 +0900

    Add files via upload

--- Model/Main.py
+++ Model/Main.py
@@ -6,13 +6,28 @@ class Main():
         sentiment = Sentiment_Analysis()
         sentiment.start_train()
 
-        print("어뷰징 리뷰를 판별하는 프로그램입니다.")
-        review = input('리뷰를 입력하세요: ')
-        star = input('별점을 입력하세요: ')
+        print("어뷰징 리뷰를 판별하는 프로그램입니다.(종료시 0 입력)")
+        while(1):
+            review = input('리뷰를 입력하세요: ')
+            
+            if review == '0':
+                break
 
-        result = sentiment.start_test(review)
+            star = input('별점을 입력하세요: ')
+            
+            result = sentiment.start_test(review)
+            
+            #별점 테러, 별점과 리뷰의 불일치
+            if (result == '긍정 리뷰') and (star == '1' or star == '2'):
+                result = '허위 리뷰'
+            elif (result =='부정 리뷰') and (star == '4' or star == '5'):
+                result = '허위 리뷰'
+            elif result == '부정 리뷰' and review.find('배달'):
+                result = '허위 리뷰'
+            else:
+                result = '정상 리뷰'
 
-        print(result)
+            print(result)
 
     if __name__=="__main__": 
         main()
