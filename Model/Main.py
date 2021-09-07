6c60e420f9122909852ad8f102c76d8635b68a4a
jini_11 <78905126+jini11@users.noreply.github.com>
2021-09-07 23:20:14 +0900

    Add files via upload

--- Model/Main.py
+++ Model/Main.py
@@ -1,10 +1,12 @@
 from Sentiment_Analysis import Sentiment_Analysis
-
+from Preprocess import Preporcess
 
 class Main():
     def main():
         sentiment = Sentiment_Analysis()
         sentiment.start_train()
+
+        preprocess = Preporcess()
 
         print("어뷰징 리뷰를 판별하는 프로그램입니다.(종료시 0 입력)")
         while(1):
@@ -15,15 +17,23 @@ class Main():
 
             star = input('별점을 입력하세요: ')
             
+            #데이터 전처리(띄어쓰기, 맞춤법, 자모음 삭제)
+            review = preprocess.spacing(review)
+            review = preprocess.dele(review)
+
+            print(review)
+
             result = sentiment.start_test(review)
             
             #별점 테러, 별점과 리뷰의 불일치
             if (result == '긍정 리뷰') and (star == '1' or star == '2'):
-                result = '허위 리뷰'
+                result = '악성 리뷰'
             elif (result =='부정 리뷰') and (star == '4' or star == '5'):
-                result = '허위 리뷰'
-            elif result == '부정 리뷰' and review.find('배달'):
-                result = '허위 리뷰'
+                result = '악성 리뷰'
+            elif (result == '부정 리뷰' or result == '악성 리뷰') and review.find('배달'):
+                result = '악성 리뷰'
+            elif result == '악성 리뷰':
+                result = '악성 리뷰'
             else:
                 result = '정상 리뷰'
 
