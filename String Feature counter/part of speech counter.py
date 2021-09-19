aa744f927f07ca1a5c6941344e6168f60bd7fb4c
BeefLover <bejm1997@gmail.com>
2021-09-20 00:25:49 +0900

    클래스로 변경 및 포팅

--- String Feature counter/part of speech counter.py
+++ String Feature counter/part of speech counter.py
@@ -1,63 +1,37 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 from konlpy.tag import Komoran
 import pandas as pd
 from pandas import DataFrame
 
+class pscCounter:
+    def counter(review_text):
 
-# In[2]:
+        nlpData = Komoran.pos(review_text)
 
+        rawData = pd.DataFrame(nlpData)
 
-reviewText = input("리뷰를 입력하시오 : ")
+        rawData.columns = ["words", "PSC"]
 
+        counterList = rawData['PSC']
 
-# In[3]:
+        targetA = ['NNG', 'NNP', 'NNB', 'VA','JKS', 'JKC', 'JKG','JKO','JKB','JKV','JKQ','JX','JC','MM']
+        targetB = ['VV', 'MAG', 'MAJ', 'NP', 'NR']
 
+        a, b = (0, 0)
 
-Komoran = Komoran()
+        for index in counterList:
+            if index in targetA:
+                a+=1
+            elif index in targetB:
+                b+=1
+        
+        total = len(rawData)
 
-nlpData = Komoran.pos(reviewText)
+        targetApercent = (a/total)*100
+        targetBpercent = (b/total)*100
 
-
-# In[4]:
-
-
-rawData = pd.DataFrame(nlpData)
-
-rawData.columns = ["words", "PSC"]
-
-counterList = rawData['PSC']
-
-# print(counterList)
-
-
-# In[5]:
-
-
-a, b = (0, 0)
-
-targetA = ['NNG', 'NNP', 'NNB', 'VA','JKS', 'JKC', 'JKG','JKO','JKB','JKV','JKQ','JX','JC','MM']
-targetB = ['VV', 'MAG', 'MAJ', 'NP', 'NR']
-
-for index in counterList:
-    if index in targetA:
-        a+=1
-    elif index in targetB:
-        b+=1
-
-
-# In[12]:
-
-
-total = len(rawData)
-
-targetApercent = (a/total)*100
-targetBpercent = (b/total)*100
-
-# print(targetApercent)
-# print(targetBpercent)
-
+        if(targetApercent < targetBpercent):
+            return 1
+        elif(targetBpercent < targetApercent):
+            return -1
+        else:
+            return 0
