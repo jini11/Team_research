03e7a9511480739e0b7eb8176d0a8c3227ac814d
jini_11 <78905126+jini11@users.noreply.github.com>
2022-01-09 16:56:54 +0900

    Add files via upload

--- 최종모델/Main.py
+++ 최종모델/Main.py
@@ -18,15 +18,15 @@ class Main():
         test_x = df['리뷰']
         test_star = df['별점']
 
-        #print("어뷰징 리뷰를 판별하는 프로그램입니다.(종료시 0 입력)")
+        print("이상 리뷰를 판별하는 프로그램입니다.(종료시 0 입력)")
         #while(1):
         for review, star in zip(test_x,test_star):
-            #review = input('리뷰를 입력하세요: ')
+            review = input('리뷰를 입력하세요: ')
             
-            #if review == '0':
-            #    break
+            if review == '0':
+                break
 
-            #star = input('별점을 입력하세요: ')
+            star = input('별점을 입력하세요: ')
              
             #데이터 전처리(띄어쓰기, 맞춤법, 자모음 삭제)
             review = preprocess.spacing(review)
@@ -36,7 +36,7 @@ class Main():
             #감성분석 실시해 세부적으로 어뷰징 리뷰 분류
             sen_score, result = sentiment.start_test(review)
         
-            print(result)
+            #print(result)
             if(review == ''):
                 review = '빈칸'
 
@@ -50,18 +50,18 @@ class Main():
 
             #별점 테러, 별점과 리뷰의 불일치
             if (result == '정상 리뷰') and (star == '1' or star == '2'):
-                result = '어뷰징 리뷰'
+                result = '이상 리뷰'
             elif (result =='악성 리뷰') and (star == '4' or star == '5'):
-                result = '어뷰징 리뷰'
+                result = '이상 리뷰'
             elif result == '악성 리뷰':
-                result = '어뷰징 리뷰'
+                result = '이상 리뷰'
             #else:
             #    result = '정상 리뷰'
 
             print('이 리뷰는 약 {0:.2f}%로 {1}로 예상됩니다.'.format(final_score, result))
             
             f = open('C:/Users/User/OneDrive - 공주대학교/바탕 화면/review/model_addition.txt',mode='a', encoding='utf-8' )
-            if(result == '어뷰징 리뷰'):
+            if(result == '이상 리뷰'):
                 f.write("1\n")
             elif(result == '정상 리뷰'):
                 f.write("0\n")
